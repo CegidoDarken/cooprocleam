23943ba4b5e4f38fcd3a1c25410bf6cfae8a95e1
CegidoDarken <71031945+CegidoDarken@users.noreply.github.com>
2020-10-26 18:53:11 -0500

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <script src="index.js"></script>
+  <title>Document</title>
+</head>
+<body>
+<?php
+$serverName = "serverName\sqlexpress"; //serverName\instanceName
+$connectionInfo = array( "Database"=>"dbName", "UID"=>"userName", "PWD"=>"password");
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
+
+if( $conn ) {
+     echo "Conexión establecida.<br />";
+}else{
+     echo "Conexión no se pudo establecer.<br />";
+     die( print_r( sqlsrv_errors(), true));
+}
+?>
+</body>
+</html>
