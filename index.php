339fac8b81dff36eaec46b7699b97635aea2676e
CegidoDarken <71031945+CegidoDarken@users.noreply.github.com>
2020-10-26 18:51:28 -0500

    Update index.php

--- index.php
+++ index.php
@@ -7,6 +7,17 @@
   <title>Document</title>
 </head>
 <body>
+<?php
+$serverName = "serverName\sqlexpress"; //serverName\instanceName
+$connectionInfo = array( "Database"=>"dbName", "UID"=>"userName", "PWD"=>"password");
+$conn = sqlsrv_connect( $serverName, $connectionInfo);
 
+if( $conn ) {
+     echo "Conexión establecida.<br />";
+}else{
+     echo "Conexión no se pudo establecer.<br />";
+     die( print_r( sqlsrv_errors(), true));
+}
+?>
 </body>
 </html>
